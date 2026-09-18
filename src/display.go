d692faa74d1388c32cb91849001c2e1101723969
Vivien CHAUDRON <vivien.chaudron@ynov.com>
2026-09-18 16:49:03 +0200

    feat: ajout du Display pour dialogue et narration.

--- src/display.go
+++ src/display.go
@@ -1,2 +1,16 @@
 package src
 
+import "fmt"
+
+var icons = map[string]string{"chat": "ᓚᘏᗢ", "joueur": "𐀪"}
+
+func DisplayDialogue(icon string, m string) {
+	fmt.Println("[{(--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--)}]\n")
+	fmt.Printf("|[ %s ]|\n\t<< %s >>\n", icons[icon], m)
+}
+
+func DisplayNarration(m string){
+	fmt.Println("[{(--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--__--)}]\n")
+	fmt.Printf("[[ %s ]]\n", m)
+}
+
