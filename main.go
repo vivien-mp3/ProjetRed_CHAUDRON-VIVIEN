25be4df63352b9fcf3405a1bd6a98c628f143734
Mathieu JULIEN <mathieu.julien@ynov.com>
2026-09-21 11:59:15 +0200

    feat : ajout saisi pour le choix du perso et sont nom dans le main

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"projet/src"
+	"strings"
 )
 
 type character struct {
@@ -11,7 +15,24 @@ type character struct {
 func main() {
 	//permet initialiser le menu
 	src.StartMenu()
-	// permet de lancer la narration 
+	//choix du type de personnage
+	fmt.Println("Veuillez Saisir le type de personnage entre les différents proposé :")
+	fmt.Println("1 - Humain || 2 - chartaceus || 3 - plantyrien")
+	var typesaisi string
+	firstvalue := bufio.NewScanner(os.Stdin)
+	firstvalue.Scan()
+	typesaisi = strings.TrimSpace(firstvalue.Text())
+	//choix du nom du personnage
+	fmt.Println("Veuillez Saisir le nom de votre personnage entre les différents proposé :")
+	var nomsaisi string
+	secondvalue := bufio.NewScanner(os.Stdin)
+	secondvalue.Scan()
+	nomsaisi = strings.TrimSpace(secondvalue.Text())
+	//information sur le personnage
+	var test src.Character
+	test.InitCharacter(nomsaisi, typesaisi)
+	fmt.Println(test)
+	// permet de lancer la narration
 	src.DisplayDialogue("chat", "Le chat", "Je suis le meow")
 	src.DisplayDialogue("joueur", "Le joueur", "Oe oe oe !")
 	mess := "Un vent froid coule sur votre peau.\n\tVous vous sentez bizarre, le front chaud, mais le corps froid.\n\tCela semble comme un cauchemard."
--- src/Menu.go
+++ src/Menu.go
@@ -2,35 +2,35 @@ package src
 
 //Code permetant la création d'un menu start, qui permet de choisir le personnage et de lancer le jeu
 
+import (
+	"fmt"
+	"os"
+)
 
-	import (
-		"fmt"
-		"os"
-	)
-
-	func StartMenu(){
-		//code permetant d'arriver sur le jeu .
-		var choice int
-		for result := true ; result; {
+func StartMenu() {
+	//code permetant d'arriver sur le jeu .
+	var choice int
+	for result := true; result; {
 		fmt.Println("Bienvenue dans le jeu!")
 		fmt.Println("1 : Commencer une nouvelle partie")
 		fmt.Println("2 : Charger une partie")
 		fmt.Println("3 : Quitter le jeu")
 		fmt.Print("Veuillez entrer votre choix (1, 2 ou 3) : ")
 		fmt.Scanln(&choice)
-		
+
 		switch choice {
 
 		case 1:
 			fmt.Println("Nouvelle partie commencée !")
-		case 2 :
+			result = false
+		case 2:
 			fmt.Println("Parametre")
-		case 3 : 
+		case 3:
 			fmt.Println("Vous avez quittez le jeu")
 			result = false
 			os.Exit(0)
-		default :
-		fmt.Println("Oups tromper de touche\n")
-		}		
-	}	
+		default:
+			fmt.Println("Oups tromper de touche\n")
+		}
+	}
 }
