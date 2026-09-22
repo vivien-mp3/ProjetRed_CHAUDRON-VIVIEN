f1310c235e6aa50eca6a124e6582048fdbce149d
Mathieu JULIEN <mathieu.julien@ynov.com>
2026-09-22 14:38:57 +0200

    fix style character.go

--- src/player/character.go
+++ src/player/character.go
@@ -19,11 +19,10 @@ type Character struct {
 	INV   map[string]int
 }
 
-
 // défini les stats du personnage correspondant au personnage choisi, choix possible avec le nom ou le numéro du personnage
 func (a *Character) InitCharacter(name string, types int) {
 	a.NAME = name
-	switch types{
+	switch types {
 	case 0:
 		a.TYPE = "humain"
 		a.PVMAX = 100
@@ -64,10 +63,10 @@ var test Character
 
 func EnterName() string {
 	for true {
-		
+
 		fmt.Println("\nVeuillez Saisir votre nom:")
 		NomSaisi = bubble.StartInput("\tJe m'appelle....\t")
-		fmt.Println("\nVotre nom est donc: ", NomSaisi, "\n")
+		fmt.Println("\nVotre nom est donc: ", NomSaisi)
 
 		return NomSaisi
 	}
@@ -77,9 +76,9 @@ func EnterName() string {
 func EnterType() int {
 	common.DisplayNarration(("Bienvenue à bord de votre vaisseau " + NomSaisi + ". Vous êtes entré(e) dans un monde où nul à de logique."))
 
-		TypeSaisi := bubble.StartChoice([]string{"Humain.[Personnage par défaut, rien de spécial.]", 
+	TypeSaisi := bubble.StartChoice([]string{"Humain.[Personnage par défaut, rien de spécial.]",
 		"Chartaceus.[Un Homme-papier, faible mais coupe plus facilment.]",
-		"Plantyrien.[Une plante qui aspire les nutriments d'autrui. Peut survivre plus longtemps.]",}, false)
+		"Plantyrien.[Une plante qui aspire les nutriments d'autrui. Peut survivre plus longtemps.]"}, false)
 
-		return TypeSaisi
+	return TypeSaisi
 }
