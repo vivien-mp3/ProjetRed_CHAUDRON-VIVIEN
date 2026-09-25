27506b1abb992da2e04e5b74d4065d36871c0a22
Vivien CHAUDRON <vivien.chaudron@ynov.com>
2026-09-25 12:06:27 +0200

    fix: main.go

--- main.go
+++ main.go
@@ -34,23 +34,9 @@ func main() {
 	// permet de lancer la narration
 	common.DisplayInfo(plr.NAME, plr.TYPE, plr.PVMAX, plr.PV, plr.ATK, plr.DEF, plr.MONNAIE)
 
-	time.Sleep(5 * time.Second)
-
 	common.DisplayDialogue("robot", "I.A Vaisseau", "Alerte! Alerte! Crash imminent, préparez vous à la collision dans... 3.. 2.. 1.")
 	
-	time.Sleep(5 * time.Second)
+	time.Sleep(3 * time.Second)
 
 	story.BeginAdventure(&plr)
-
-	time.Sleep(1 * time.Second)
-	common.DisplayDialogue("chat", "Le chat", "Je suis le meow")
-	time.Sleep(2 * time.Second)
-	common.DisplayDialogue("joueur", player.NomSaisi, "Oestory. oe oe !")
-	time.Sleep(1 * time.Second)
-	mess := "Un vent froid coule sur votre peau.\n\tVous vous sentez bizarre, le front chaud, mais le corps froid.\n\tCela semble comme un cauchemard."
-	common.DisplayNarration(mess)
-	src.Carte(&plr)
-	// Rencontre avec le Marchand
-	economie.Boutique(&plr)
-	player.AccesInventory(&plr.INV)
 }
